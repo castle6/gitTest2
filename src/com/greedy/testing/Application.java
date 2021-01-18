fa072d4612a0e0a274c5b2b92b5f4df4e9cc40a8
tj <tj@DESKTOP-MOB8F55>
2021-01-18 11:27:41 +0900

    202101811_second commit
    메인에 출력문 추가

--- src/com/greedy/testing/Application.java
+++ src/com/greedy/testing/Application.java
@@ -7,6 +7,9 @@ public class Application {
 		
 		
 		System.out.println("Hello World"); 
+		System.out.println("Hello World"); 
+		System.out.println("Hello World"); 
+		System.out.println("Hello World"); 
 		
 		
 	}
